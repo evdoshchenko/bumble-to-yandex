0b5a64da95184d1bd84db5fcada5dd522e52f6b7
Danil <ev8ko@icloud.com>
2023-02-16 03:37:44 +0400

    assets js format

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
+        entryFileNames: `assets/[name].js`, format: 'umd',
         chunkFileNames: `assets/[name].js`,
         assetFileNames: `assets/[name].[ext]`,
       },
